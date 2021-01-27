e90ae82d911bedc306c30ff3e1fd5e5464848e75
Steve Freeland <stevefreeland1@gmail.com>
2021-01-26 19:56:00 -0500

    reorganized project folders

--- auto_insurance/config.bat
+++
@@ -1,2 +0,0 @@
-SET TABLE_NAME=insurance_claims
-SET "DATABASE_URL=postgres://postgres:slfpostgres2435!@localhost:5432/auto_insurance"
---
+++ config.bat
@@ -0,0 +1,2 @@
+SET TABLE_NAME=insurance_claims
+SET "DATABASE_URL=postgres://postgres:slfpostgres2435!@localhost:5432/machine_learning_project"
